749fcf18ceef59d8ecebac159aa4ade4ba41276a
luizaru <73501981+luizaru@users.noreply.github.com>
2023-01-27 13:49:46 +0000

    Update Layout.js

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import Footer from '../footer/Footer';
+import Header from '../header/Header';
 
+const layout = ({children}) => {
+  return (
+    <>
+        <Header />
+        <div style={{minHeight: "80vh" }} className="--pad">
+            {children}
+        </div>
+
+        <Footer />
+        
+    </>
+
+
+  )
+}
+
+export default layout
